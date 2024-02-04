bf78b20ca99c837f14b7282e87cd362846d090e7
OsamaMalik651 <osama.malik013@gmai.com>
2024-02-04 14:19:22 -0700

    created History Page, created List Item component

--- client/src/Pages/HistoryPage/index.js
+++ client/src/Pages/HistoryPage/index.js
@@ -1,7 +1,72 @@
-import React from 'react'
+import React, { Fragment } from 'react';
+import ListItem from '../../components/ListItem';
+
+
 const HistoryPage = () => {
+
+    // static data.
+    let shoppingLists = [{
+        id: 1,
+        title: "Grocery List",
+        createdOn: new Date('8/27/2020'),
+        status: "completed"
+    }, {
+        id: 2,
+        title: "Eero's farewell party",
+        createdOn: new Date('8/24/2020'),
+        status: "completed"
+    },
+    {
+        id: 3,
+        title: "Board game week 2",
+        createdOn: new Date('7/27/2021'),
+        status: "completed"
+    },
+    {
+        id: 4,
+        title: "Grocery List",
+        createdOn: new Date('7/16/2021'),
+        status: "cancelled"
+    },
+    ];
+    // data from backend will be received as this function's output
+    var groupedShoppingList = shoppingLists.reduce((group, shoppingList) => {
+        const groupDate = `${shoppingList.createdOn.getMonth()}-${shoppingList.createdOn.getFullYear()}`;
+        if (!group[groupDate]) {
+            group[groupDate] = [];
+        }
+        group[groupDate].push(shoppingList);
+        return group;
+    }, {});
+
+    var renderedShoppingLists = Object.keys(groupedShoppingList).map((key) => {
+        var shoppingListGroup = groupedShoppingList[key];
+        var groupTitle = new Date(shoppingListGroup[0].createdOn).toLocaleDateString('en-us', { month: 'short', year: 'numeric' });
+        return (<div key={key} className='flex flex-1 flex-col gap-4'>
+            <div className='text-xs font-medium'>
+                {groupTitle}
+            </div>
+            <div className='flex flex-col gap-7'>
+                {shoppingListGroup.map((shoppingList) => {
+                    return <ListItem shoppingList={shoppingList} />
+                })}
+            </div>
+        </div>
+        )
+    });
     return (
-        <div>HistoryPage</div>
+        <div className='flex flex-1'>
+            <div className='flex flex-1 flex-col gap-12 ps-20 pe-14 py-7 bg-pageBackground'>
+                <div className='text-[26px] max-w-[450px] font-medium'>
+                    <h1>Shopping History</h1>
+                </div>
+                <div className='flex flex-1 flex-col flex-wrap gap-12'>
+                    {renderedShoppingLists}
+                </div>
+            </div>
+            <div className=' max-w-[390px] flex flex-col basis-1/3' ></div>
+
+        </div>
     )
 }
 
---
+++ client/src/components/ListItem/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { MdOutlineEventNote } from "react-icons/md";
+import { MdKeyboardArrowRight } from "react-icons/md";
+const ListItem = ({ shoppingList }) => {
+    return (
+        <div className='ps-5 pe-1 py-2 flex items-stretch h-16 max-w-4xl bg-white text-base rounded-lg shadow-md' key={shoppingList.id}>
+            <div className='flex flex-1 items-center justify-between'>
+                <div className="text-base font-medium flex-1">{shoppingList.title}</div>
+                <div className="flex flex-1 gap-5 justify-end items-end">
+                    <div className='text-iconGray flex flex-1 max-w-[140px] gap-2 text-sm items-center'>
+                        <MdOutlineEventNote className='w-6 h-6' />
+                        <p>{shoppingList.createdOn.toLocaleDateString('en-us', { weekday: 'short' })} {shoppingList.createdOn.toLocaleDateString('en-GB').replace(/\//g, '.')}</p>
+                    </div>
+                    <div className={`max-w-[76px] h-6 px-2 py-1 border flex flex-1 rounded-md text-xs items-center justify-center ${shoppingList.status === 'completed' && 'border-info text-info'} ${shoppingList.status === 'cancelled' && 'border-danger text-danger'}`} >
+                        {shoppingList.status}
+                    </div>
+                    <div className=' text-bgOrange'>
+                        <MdKeyboardArrowRight className='font-bold h-6 w-6 cursor-pointer' />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ListItem
